ce5ebfcca87514a5124386a15771ac0c0e2ba91f
Gilberto-Filho <tiozing2@gmail.com>
2019-10-02 14:39:05 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-sx3k1c
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-sx3k1c)
---
+++ src/app/model.ts
@@ -0,0 +1,20 @@
+export class Model{
+  user;
+  items;
+  constructor(){
+    this.user="Gilberto";
+    this.items=[]
+}
+}
+export class TodoItem{
+  action;
+  done;
+  priority;
+  data: Date;
+  constructor(action, done, priority, data){
+    this.action=action;
+    this.done=done;
+    this.priority=priority;
+    this.data=data;
+  }
+}
